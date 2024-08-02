9879faf55c5fd419285f49a327ec197e6180ec90
shifamahveen <53911226+shifamahveen@users.noreply.github.com>
2024-08-02 11:18:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// let heading = document.getElementsByTagName("h1");
+// console.log(heading);
+
+// let para = document.getElementsByClassName("demo");
+// console.log(para)
+
+// let section = document.getElementById("sample")
+// console.log(section);
+
+// let heading = document.querySelector("h1")
+// console.log(heading);
+
+let para = document.querySelector(".demo");
+console.log(para);
+
+let section = document.querySelector("#sample")
+console.log(section)
